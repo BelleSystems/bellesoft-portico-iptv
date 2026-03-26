73466f4077640720517e25aa5c4fde2e17f23fdf
Teds30 <teddymarcenaje@gmail.com>
2026-03-26 17:25:52 +0800

    Refactor API routes to include 'external-api' prefix for IPTV endpoints

--- src/routes/api.php
+++ src/routes/api.php
@@ -5,7 +5,9 @@ use Bellesoft\PorticoIptv\Models\Room;
 use Illuminate\Support\Facades\Route;
 use Bellesoft\PorticoIptv\Controllers\RoomController;
 
-Route::prefix('iptv')->group(function () {
-    Route::get('/rooms/{room}/current-reservation', [RoomController::class, 'currentReservation'])
-        ->name('rooms.current-reservation');
+Route::prefix('external-api')->group(function () {
+    Route::prefix('iptv')->group(function () {
+        Route::get('/rooms/{room}/current-reservation', [RoomController::class, 'currentReservation'])
+            ->name('rooms.current-reservation');
+    });
 });
